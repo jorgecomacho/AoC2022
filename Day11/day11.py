c10df04fc7c18a7489f59ce0290dad8cef507dda
jorgecomacho <119549110+jorgecomacho@users.noreply.github.com>
2022-12-11 11:26:19 -0700

    Update day11.py

--- Day11/day11.py
+++ Day11/day11.py
@@ -1,85 +1,65 @@
-#filename = 'day11.txt'
-filename = 'test.txt'
+filename = 'day11.txt'
+#filename = 'test.txt'
 
-import copy
+import copy, os
 
 def read_file(fname):
-    f = open(fname,"r")
+    directory = os.path.dirname(os.path.realpath(__file__))
+    full_path = '/'.join([directory, fname])
+    f = open(full_path,"r")
     lines = [i.strip() for i in f.readlines()]
     return lines
 
-def parse_monkeys(lines):
+def monkey_parts(lines):
     monkeys = []
-    temp_dict = {}
-    i = 0
-    for line in lines:
-        if 'Monkey' in line:
-            i = int(line.split()[1].split(':')[0])
-            continue
-        if 'Starting items' in line:
-            temp_dict['starting'] = [int(i) for i in line.split(': ')[1].split(', ')]
-            continue
-        if 'Operation' in line:
-            temp_dict['op'] = line.split(': ')[1].split()[-2:]
-            continue
-        if 'Test' in line:
-            temp_dict['divisor'] = int(line.split()[-1])
-            continue
-        if 'true' in line:
-            temp_dict['true'] = int(line.split()[-1])
-            continue
-        if 'false' in line:
-            temp_dict['false'] = int(line.split()[-1])
-            temp_dict['inspect'] = 0
-            monkeys.append(copy.deepcopy(temp_dict))
-            continue
-    return monkeys
+    master_divisor = 1
+    for i in range(1, len(lines), 7):
+        temp_dict = {}
+        temp_dict['items'] = [int(x) for x in lines[i].split(': ')[1].split(', ')]
+        temp_dict['op'] = eval(f'lambda old: {lines[i+1].split("= ")[1]}')
+        temp_dict['divisor'] = int(lines[i+2].split()[-1])
+        master_divisor *= temp_dict['divisor']
+        temp_dict['true'] = int(lines[i+3].split()[-1])
+        temp_dict['false'] = int(lines[i+4].split()[-1])
+        temp_dict['count'] = 0
+        monkeys.append(temp_dict)
+    return monkeys, master_divisor
 
-def print_monkeys(monkeys):
+def show_me_the_monkey(monkeys):
+    counts = []
     for monkey in monkeys:
-        for i in monkey.keys():
-            print(i, monkey[i])
+        for entry in monkey.keys():
+            print(f'{entry}: {monkey[entry]}')
         print()
+        counts.append(monkey['count'])
+    counts.sort()
+    print(counts[-1] * counts[-2])
 
-def monkey_rounds(monkeys, worry_adjust):
-    for i in range(len(monkeys)):
-        for j in range(len(monkeys[i]['starting'])):
-            worry = monkeys[i]['starting'][0]
-            monkeys[i]['inspect'] += 1
-            new = worry
-            if '+' in monkeys[i]['op'][0]:
-                new = worry + int(monkeys[i]['op'][1])
-            elif 'old' == monkeys[i]['op'][1]:
-                new = worry * worry
-            else:
-                new = worry * int(monkeys[i]['op'][1])
-            #new = int(new/worry_adjust)
-            if new % monkeys[i]['divisor'] == 0:
-                monkeys[i]['starting'].pop(0)
-                monkeys[monkeys[i]['true']]['starting'].append(new)
-            else:
-                monkeys[i]['starting'].pop(0)
-                monkeys[monkeys[i]['false']]['starting'].append(new)
-    return monkeys
-
-def two_most(monkeys):
-    tries = []
+def do_monkey_stuff(monkeys, mega_mod=None, divisor=1):
     for monkey in monkeys:
-        tries.append(monkey['inspect'])
-    tries.sort()
-    print(tries[-1]*tries[-2])
+        for item in monkey['items']:
+            monkey['count'] += 1
+            new = monkey['op'](item)
+            new = int(new / divisor)
+            if mega_mod:
+                new %= mega_mod
+            test = new % monkey['divisor'] == 0
+            if test:
+                monkeys[monkey['true']]['items'].append(new)
+            else:
+                monkeys[monkey['false']]['items'].append(new)
+        for i in range(len(monkey['items'])):
+            monkey['items'].pop(0)
+    return monkeys
 
 if __name__ == "__main__":
     lines = read_file(filename)
-    monkeys = parse_monkeys(lines)
+    part1, _ = monkey_parts(lines)
+    for i in range(20):
+        part1 = do_monkey_stuff(part1, divisor=3)
+    show_me_the_monkey(part1)
 
-    #for i in range(20):
-    #   monkeys = monkey_rounds(monkeys,3) #monkey
-
-    for j in range(100):
-        for i in range(100):
-            monkeys = monkey_rounds(monkeys,1) #monkey
-        print(j)
-    print_monkeys(monkeys)
-    two_most(monkeys)
-
+    part2, master = monkey_parts(lines)
+    for i in range(10000):
+        part2 = do_monkey_stuff(part2, mega_mod=master)
+    show_me_the_monkey(part2)
